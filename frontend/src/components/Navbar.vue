a78d20a52b9027ba5bbf6f92d6b9e7edabb8d11e
julienb86 <julien.bouafia@gmail.com>
2019-05-09 09:52:58 +0200

    implemented the login and signup template and linked the pages

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <a class="navbar-brand" href="#">
-    <img src="../assets/logo.png" alt="logo" width="30" height="30">
+    <img src="../assets/icon.png" alt="logo" width="75" height="75">
   </a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -9,13 +9,15 @@
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav ml-auto">
+      <router-link class="nav-link" to="/login">Login</router-link>
+      <router-link class="nav-link" to="/signup">Signup</router-link>
       <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+ <!--        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Groupomania
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
           <a class="dropdown-item" href="#">Logout</a>
-        </div>
+        </div> -->
       </li>
     </ul>
   </div>
--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,3 +1,18 @@
 <template>
-    <h1>Login Component</h1>
+    <div class="row">
+        <div class="card col-md-6 offset-md-3 my-5">  
+            <div class="card-body">
+                <h3 class="text-center my-3">Login</h3>
+                <div class="form-group ">
+                    <input type="text" placeholder="Email" class="form-control">
+                </div>
+                <div class="form-group ">
+                    <input type="password" placeholder="Password" class="form-control">
+                </div>
+                <div class="form-group">
+                    <button class="btn btn-success form-control">Login</button>                    
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
