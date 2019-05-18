49d2161cb729a8bed26e3651afdaf88afab63c08
julienb86 <julien.bouafia@gmail.com>
2019-05-18 13:15:31 +0200

    changed the path for the routes

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -10,7 +10,7 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav ml-auto">
       <li class="nav-item" v-if="!isAuth"> 
-        <router-link class="nav-link" to="/login" >Login</router-link>
+        <router-link class="nav-link" to="/" >Login</router-link>
       </li>
       <li class="nav-item" v-if="!isAuth">
         <router-link class="nav-link" to="/signup">Signup</router-link>
