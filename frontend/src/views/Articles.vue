ec448358eeef1e72d1ea85da8a595914b8eaccd4
julienb86 <julien.bouafia@gmail.com>
2019-05-22 11:11:18 +0200

    implemented the publish page for the frontend

--- frontend/src/views/Articles.vue
+++ frontend/src/views/Articles.vue
@@ -1,26 +1,3 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="card col-md-8 offset-md-2 my-5">
-                <div class="card-body">
-                    <wysiwyg v-model="content" />
-                    <div class="text-center my-3">
-                        <button class="btn btn-primary btn-lg">Send</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    
+    <h1>Welcome</h1>
 </template>
-
-
-<script>
-export default {
-    data(){
-        return {
-            content: ''
-        }
-    }
-}
-</script>
