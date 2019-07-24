662a2c4d5cd44b72b33ebf63ee8fcd7342672be6
julienb86 <julien.bouafia@gmail.com>
2019-07-24 14:15:56 +0200

    project finished

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,8 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const userCtrl = require('../controllers/user');
-const auth = require('../middleware/auth')
-/* add auth below */
+
 
 router.post('/signup', userCtrl.signup);
 router.post('/login',userCtrl.login);
